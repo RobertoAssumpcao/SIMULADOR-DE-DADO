d879cabe7182f714033eee49c8ba95606ba71e12
Roberto de Paiva Assumpção <robertopaiva26@outlook.com>
2021-12-12 23:28:10 -0300

    Menu e numero
    Melhorando o menu, exclui o segundo e acrescentei o random

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
-# Verificando se o usuario gostaria de jogar.
-jogar = input("Você gostaria de jogar o dado? Digite Y/N\n") #tratar erro
-if jogar.upper() == "Y":
+import random
+# menu
+menu = True
+while menu is True:
+
+    # Verificando se o usuario gostaria de jogar.
+    jogar = str(input("Você gostaria de jogar o dado? Digite Y/N\n")) #tratar erro
+    if jogar.upper() == "Y":
+        dado = random.randint(1,6)
+        print(f'O numero do dado foi {dado:.0f}')
+    # jogo não começa
+    elif jogar.upper() == "N":
+        print("Uma pena, poderia ter nos divertido com as apostas =)\n")
+        menu = False
+    else:
+        print("digite uma opção Valida!\n")
     
-  """  # Começando jogo.
-    comeca_jogo = True
-    while comeca_jogo is True:
-        
-
-        #verificando se o usuario gostaria de sair do jogo.
-        print("Digite '1' para continuar.\n")
-        print("Digite '0' para sair.\n")
-        continua_jogo = int(input("Qual sua resposta ?\n")) #tratar erro
-
-        # Jogo continua
-        if continua_jogo == 1:
-            comeca_jogo = True
-        # jogo acaba
-        elif continua_jogo == 0:
-            comeca_jogo = False
-            print("Saindo do jogo!")
-        # Tratando erro
-        else:
-            print("Digite uma opção valida")
-"""
-# jogo não começa
-else:
-    print("Uma pena, poderia ter nos divertido com as apostas =)\n")
